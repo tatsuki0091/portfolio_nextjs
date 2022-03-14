e55ef4b510880f0c5498882f56e7ff5d79880f93
Tatsuki Yamatani <yao099991@gmail.com>
2022-03-13 20:07:01 -0700

    Finished to create context

--- components/About.tsx
+++ components/About.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import profile from "../public/img/profile.jpg";
-import { Button } from "../styles/aboutStyle";
-import Link from "next/link";
+// import { Button } from "../styles/aboutStyle";
+// import Link from "next/link";
 import dynamic from "next/dynamic";
-import ParticlesBg from "particles-bg";
+import { useContext } from "react";
+import { DataContext } from "./contexts/DataContext";
 
 const About = () => {
+  const mountedContext = useContext(DataContext);
   const ParticlesBg = dynamic(
     () => {
       return import("particles-bg");
@@ -16,7 +18,16 @@ const About = () => {
 
   return (
     <>
-      <ParticlesBg color="#dc2626" num={200} type="cobweb" bg={true} />
+      {mountedContext.mounted == true ? (
+        <>
+          <ParticlesBg color={"#4f4f4f"} num={200} type="cobweb" bg={true} />
+        </>
+      ) : (
+        <>
+          <ParticlesBg color={"#dbdbdb"} num={200} type="cobweb" bg={true} />
+        </>
+      )}
+
       <div className="container px-4 mx-auto">
         <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
           <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
@@ -31,20 +42,26 @@ const About = () => {
             />
           </div>
           <div className="lg:px-4 lg:mt-12 ">
-            <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+            <h1 className="text-2xl font-bold lg:text-5xl dark:text-white">
               Hello
             </h1>
-            <div className="mt-6 text-gray-800 dark:text-white">
+            <div className="mt-6 text-lg dark:text-white">
               <p className="mb-4">
-                I'm Tatsuki who has 4 years experiences as a Fullstack Developer
+                I'm Tatsuki who has 4 years experience as a Fullstack Developer
                 specialized in both front-end and back-end web development.
               </p>
-              <p>I can help you with all the sides of your project:</p>
-              <p> I'm currently living in Vancouver.</p>
+              <p>I can help you with all the sides of your project.</p>
+              <p> I'm currently living in Vancouver, and looking for a job.</p>
+              <p>
+                Please push My Career link and Work link of the navigation bar
+                to check who I am.
+              </p>
+              <p> I look forward to connecting with you.</p>
+              <p> Cheers</p>
             </div>
           </div>
         </div>
-        <Link href="../pdf/resume.pdf">
+        {/* <Link href="..public/pdf/resume.pdf">
           <a
             target="_blank"
             className={
@@ -55,7 +72,7 @@ const About = () => {
               Resume
             </Button>
           </a>
-        </Link>
+        </Link> */}
       </div>
     </>
   );
