864bda07f3a97060b6b7f2a8d2d96c310ba3c740
Tatsuki Yamatani <yao099991@gmail.com>
2022-03-16 21:26:17 -0700

    Fix some navigation design

--- styles/navigationStyle.ts
+++ styles/navigationStyle.ts
@@ -3,3 +3,21 @@ import styled from "styled-components";
 export const Header = styled.header`
   width: 100%;
 `;
+
+export const HumbergerButton = styled.input.attrs({ type: "checkbox" })`
+  display: none;
+`;
+
+export const BlackLine = styled.div`
+  width: 25px;
+  height: 3px;
+  background-color: black;
+  margin: 4px;
+`;
+
+export const WhiteLine = styled.div`
+  width: 25px;
+  height: 3px;
+  background-color: white;
+  margin: 4px;
+`;
