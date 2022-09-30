de38f68b1585c881150562f73d182e92cffe6b89
Tatsuki Yamatani <yao099991@gmail.com>
2022-09-29 23:43:53 -0700

    Add json Object to show lists

---
+++ components/careers/eduInfo.js
@@ -0,0 +1,14 @@
+export const EduInfo = [
+  {
+    school: "Cornerstone International Community College of Canada",
+    term: "May 1th, 2022 - Apr 30th, 2023",
+    link: "https://ciccc.ca",
+    Majored: "-Majored in Computer Science",
+  },
+  {
+    school: "Kobe Gakuin University",
+    term: "April 1th, 2008 - March 31th, 2014",
+    link: "https://www.kobegakuin.ac.jp",
+    Majored: "-Majored in Business Administration",
+  },
+];
